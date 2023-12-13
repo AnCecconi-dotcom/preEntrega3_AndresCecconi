e32f50ed48468d8cc78f5b220471773ce18a4b1a
AndresCecconi <ancecconi15@gmail.com>
2023-12-13 15:43:00 -0300

    boton cancelar dentro seccion deposito y primera parte seccion PF

--- main.js
+++ main.js
@@ -30,10 +30,60 @@ function deposito(monto) {
     mostrarSaldo();
 }
 
+function agregarContacto() {
+    const nombre = document.getElementById('nombre').value;
+    const alias = document.getElementById('alias').value;
+    const cbu = document.getElementById('cbu').value;
+
+    // verificar si se completó al menos uno de los campos
+    if (nombre === '' && alias === '' && cbu === '') {
+        alert('Por favor, complete al menos uno de los campos (Alias o CBU).');
+        return null;
+    }
+    return {
+        nombre: nombre,
+        alias: alias,
+        cbu: cbu
+    }
+}
+
+function mostrarAgenda() {
+    const agendaElemento = document.getElementById('agenda');
+    agendaElemento.innerHTML = ''; // Limpiar contenido anterior
+    if (agenda.length === 0) {
+        // Si la agenda está vacía, mostrar un mensaje indicando que está vacía
+        agendaElemento.innerHTML = '<p>No hay contactos en la agenda.</p>';
+        return;
+    }
+
+    agenda.forEach(contacto => {
+        const itemContacto = document.createElement('div');
+        itemContacto.innerHTML = `
+        <p>Nombre: ${contacto.nombre}</p>
+        <p>Alias/CBU: ${contacto.alias} / ${contacto.cbu}</p>
+        `;
+        agendaElemento.appendChild(itemContacto);
+    });
+}
+
+//clases
+//clase Contacto
+class Contacto {
+    constructor(nombre, alias, cbu) {
+        this.nombre = nombre;
+        this.alias = alias;
+        this.cbu = cbu;
+    }
+}
+
 //variables Globales
 let usuarioRegistrado = "";
 let contraseñaRegistrada = "";
 let saldo = 0;
+let agenda = []; // array agenda contactos
+let nombre = "";
+let alias = "";
+let cbu = "";
 
 
 
@@ -44,7 +94,7 @@ document.addEventListener('DOMContentLoaded', function () {
     document.getElementById('formularioRegistro').addEventListener('submit', function (event) {
         event.preventDefault();
 
-        // Obtener los valores del formulario
+        // obtener valores del formulario
         const usuario = document.getElementById('inputUsuario').value;
         const contraseña = document.getElementById('inputContrasenia').value;
         const confirmarContrasenia = document.getElementById('inputConfirmarContrasenia').value;
@@ -53,13 +103,13 @@ document.addEventListener('DOMContentLoaded', function () {
         console.log('Contraseña:', contraseña);
         console.log('Confirmar contraseña:', confirmarContrasenia);
 
-        // Verificar campos vacíos
+        // verificar campos vacíos
         if (usuario == "" || contraseña == "" || confirmarContrasenia == "") {
             alert('Por favor, complete todos los campos.');
             return;
         }
 
-        // Verificar contraseña
+        // verificar contraseña
         if (contraseña !== confirmarContrasenia) {
             alert('Las contraseñas no coinciden. Inténtelo de nuevo.');
             return;
@@ -68,41 +118,44 @@ document.addEventListener('DOMContentLoaded', function () {
         contraseñaRegistrada = contraseña;
         alert('Registro exitoso! Ahora seras redirigido a la página principal para iniciar sesión')
 
-        // Ocultar el formulario de registro y mostrar el inicio de sesión
+        // Ooultar el formulario de registro y mostrar el inicio de sesión
         document.getElementById('registro').classList.add('oculto');
         document.getElementById('sesion').classList.remove('oculto');
     });
-    // Formulario de inicio de sesión
+    // formulario de inicio de sesión
     document.getElementById('inicioSesion').addEventListener('submit', function (event) {
         event.preventDefault();
 
-        // Obtener los valores del formulario 
+        // obtener valores del formulario 
         const usuarioIngresado = document.getElementById('nombreUsuario').value;
         const contraseñaIngresada = document.getElementById('contrasenia').value;
 
         console.log('Usuario:', usuarioIngresado);
         console.log('Contraseña:', contraseñaIngresada);
 
-        // Comparar los valores con los datos de registro
+        // comparar valores con los datos de registro
         inicioSesion(usuarioIngresado, contraseñaIngresada, usuarioRegistrado, contraseñaRegistrada);
 
         //guardar variable en local storage para recuperar luego
         if (usuarioIngresado === usuarioRegistrado && contraseñaIngresada === contraseñaRegistrada) {
-            // Almacenar el nombre de usuario en localStorage
+            // almacenar el nombre de usuario en localStorage
             localStorage.setItem('nombreUsuario', usuarioRegistrado);
 
-            // Ocultar el formulario de inicio de sesion y mostrar la pagina principal
+            // ocultar el formulario de inicio de sesion y mostrar la pagina principal
             document.getElementById('sesion').classList.add('oculto');
             document.getElementById('paginaPrincipal').classList.remove('oculto');
 
-            //Pagina Principal
-            // Mostrar el mensaje de bienvenida con el nombre de usuario y el saldo
+            //PAGINA PRINCIPAL
+            // mostrar el nombre de usuario y el saldo
             const nombreUsuario = localStorage.getItem('nombreUsuario');
             document.getElementById('mensajeBienvenida').innerText = `Hola, ${nombreUsuario}`;
 
             mostrarSaldo();
         }
     });
+
+
+    // evento click seccion deposito
     document.getElementById('botonDeposito').addEventListener('click', function () {
         //ocultar pagina principal
         document.getElementById('paginaPrincipal').classList.add('oculto');
@@ -111,28 +164,28 @@ document.addEventListener('DOMContentLoaded', function () {
         document.getElementById('seccionDeposito').classList.remove('oculto');
         mostrarSaldoEnDeposito();
     });
-
+    //evento submit formulario deposito
     document.getElementById('montoADepositar').addEventListener('submit', function (event) {
         event.preventDefault();
 
         let nuevoMonto = parseFloat(document.getElementById('monto').value);
 
         if (!isNaN(nuevoMonto) && nuevoMonto > 0) {
-            //Mensaje de confirmacion
+            //mensaje de confirmacion
             const confirmacion = confirm(`Usted esta a punto de depositar $${nuevoMonto}. Desea confirmar?`);
             if (confirmacion) {
                 deposito(nuevoMonto);
                 alert('Deposito realizado con exito!');
-                // Limpiar el campo del formulario
+                // limpiar el campo del formulario
                 document.getElementById('monto').value = '';
 
-                // Ocultar la sección de depósito después de realizar el depósito
+                // ocultar la sección de depósito después de realizar el depósito
                 document.getElementById('seccionDeposito').classList.add('oculto');
 
-                // Mostrar nuevamente la página principal
+                // mostrar nuevamente la página principal
                 document.getElementById('paginaPrincipal').classList.remove('oculto');
             } else {
-                // Limpiar el campo del formulario
+                // limpiar el campo del formulario
                 document.getElementById('monto').value = '';
                 //volver a la pagina principal
                 document.getElementById('seccionDeposito').classList.add('oculto');
@@ -140,31 +193,38 @@ document.addEventListener('DOMContentLoaded', function () {
             }
 
         } else {
-            // Monto ingresado no válido
+            // monto ingresado no válido
             alert('El monto ingresado no es válido.');
         }
     });
+    // botón "Cancelar" en la sección deposito
+    document.getElementById('cancelarDeposito').addEventListener('click', function () {
 
-     // Evento click para mostrar la sección de transferencias
-     document.getElementById('botonTransferencias').addEventListener('click', function () {
+        // volver a la página principal
+        document.getElementById('seccionDeposito').classList.add('oculto');
+        document.getElementById('paginaPrincipal').classList.remove('oculto');
+
+    });
+
+    // evento click sección de transferencias
+    document.getElementById('botonTransferencias').addEventListener('click', function () {
         document.getElementById('paginaPrincipal').classList.add('oculto');
         document.getElementById('seccionTransferencias').classList.remove('oculto');
     });
-
-     // Evento submit para el formulario de transferencias
-     document.getElementById('formularioTransferencia').addEventListener('submit', function (event) {
+    // evento submit formulario de transferencias
+    document.getElementById('formularioTransferencia').addEventListener('submit', function (event) {
         event.preventDefault();
 
         const aliasCBU = document.getElementById('aliasCBU').value;
         const montoTransferir = parseFloat(document.getElementById('montoTransferir').value);
 
-        // Verificar que el monto a transferir sea menor o igual al saldo disponible
+        // verificar que el monto a transferir sea menor o igual al saldo disponible
         if (montoTransferir <= saldo) {
-            // Verificar si el alias o CBU está en la agenda (A REALIZAR)
-            
+            // verificar si el alias o CBU está en la agenda (A REALIZAR)
+
             // confirmar la transferencia
             const confirmacion = confirm(`Usted está a punto de transferir $${montoTransferir} a ${aliasCBU}. ¿Desea confirmar la operación?`);
-            
+
             if (confirmacion) {
                 // actualizar saldo
                 saldo -= montoTransferir;
@@ -185,17 +245,94 @@ document.addEventListener('DOMContentLoaded', function () {
             alert('Fondos insuficientes para realizar la transferencia.');
         }
     });
-
-    // Botón "Cancelar" en la sección de transferencias
+    // botón "Cancelar" en la sección transferencias
     document.getElementById('cancelarTransferencia').addEventListener('click', function () {
         // Limpiar los campos del formulario
         document.getElementById('aliasCBU').value = '';
         document.getElementById('montoTransferir').value = '';
+        document.getElementById('seccionAgenda').classList.add('oculto');
 
-        // Volver a la página principal
+        // volver a la página principal
         document.getElementById('paginaPrincipal').classList.remove('oculto');
         document.getElementById('seccionTransferencias').classList.add('oculto');
     });
+
+    // boton "Ver Agenda" en la seccion trasnferencias
+    // Evento click en el botón "Agenda" para mostrar/ocultar la agenda de contactos
+    document.getElementById('botonAgenda').addEventListener('click', function () {
+        const seccionAgenda = document.getElementById('seccionAgenda');
+        if (seccionAgenda.classList.contains('oculto')){
+            seccionAgenda.classList.remove('oculto');
+            mostrarAgenda();
+        } else {
+            seccionAgenda.classList.add('oculto');
+        }
+    });
+    
+    //evento click seccion agendar contacto
+    document.getElementById('botonContacto').addEventListener('click', function () {
+        document.getElementById('paginaPrincipal').classList.add('oculto');
+        document.getElementById('seccionAgendarContacto').classList.remove('oculto');
+    });
+    //evento submit formulario contacto
+    document.getElementById('formularioContacto').addEventListener('submit', function (event) {
+        event.preventDefault();
+
+        const datosContacto = agregarContacto();
+        const { nombre, alias, cbu } = datosContacto;
+
+        // nueva instancia de la clase Contacto con los datos del formulario
+        const nuevoContacto = new Contacto(nombre, alias, cbu);
+
+        // agregar nuevo contacto al array agenda
+        agenda.push(nuevoContacto);
+        console.log(nuevoContacto);
+
+
+        // limpiar los campos del formulario después de agregar el contacto
+        document.getElementById('nombre').value = '';
+        document.getElementById('alias').value = '';
+        document.getElementById('cbu').value = '';
+
+        // ocultar el formulario de contacto
+        document.getElementById('seccionAgendarContacto').classList.add('oculto');
+        // mostrar la página principal
+        document.getElementById('paginaPrincipal').classList.remove('oculto');
+
+    });
+    // botón "Cancelar" en la sección de Contacto
+    document.getElementById('cancelarAgendar').addEventListener('click', function () {
+
+        // Limpiar los campos del formulario
+        document.getElementById('nombre').value = '';
+        document.getElementById('alias').value = '';
+        document.getElementById('cbu').value = '';
+
+        // volver a la página principal
+        document.getElementById('seccionAgendarContacto').classList.add('oculto');
+        document.getElementById('paginaPrincipal').classList.remove('oculto');
+
+    });
+
+    //evento click seccion Plazo Fijo
+
+    document.getElementById('botonPlazoFijo').addEventListener('click', function(){
+        document.getElementById('paginaPrincipal').classList.add('oculto');
+        document.getElementById('seccionPlazoFijo').classList.remove('oculto');
+
+
+  
+    });
+
+    // botón "Cancelar" en la sección PF
+    document.getElementById('cancelarPlazoFijo').addEventListener('click', function () {
+
+        // volver a la página principal
+        document.getElementById('seccionPlazoFijo').classList.add('oculto');
+        document.getElementById('paginaPrincipal').classList.remove('oculto');
+
+    });
+
 });
 
 
